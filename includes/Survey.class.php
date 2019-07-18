8cdbc844873e6107ea1507abdf77045bc48697ad
Kunal Mehta <legoktm@member.fsf.org>
2019-07-17 22:29:07 -0400

    Get rid of trailing whitespace
    My editor did this automatically, so why not.
    
    Change-Id: I1d930521804ff40c863d967375474d62af64197e

--- includes/Survey.class.php
+++ includes/Survey.class.php
@@ -2,12 +2,12 @@
 
 /**
  * Simple Survey object class.
- * 
+ *
  * @since 0.1
- * 
+ *
  * @file Survey.class.php
  * @ingroup Survey
- * 
+ *
  * @licence GNU GPL v3 or later
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
@@ -17,7 +17,7 @@ class Survey extends SurveyDBClass {
 	public static $USER_CONFIRMED = 2;
 	public static $USER_EDITOR = 3;
 	public static $USER_ANON = 4;
-	
+
 	/**
 	 * @see SurveyDBClass::getDBTable()
 	 */
@@ -28,9 +28,9 @@ class Survey extends SurveyDBClass {
 	/**
 	 * Returns an array with the fields and their types this object contains.
 	 * This corresponds directly to the fields in the database, without prefix.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @return array
 	 */
 	protected static function getFieldTypes() {
@@ -49,13 +49,13 @@ class Survey extends SurveyDBClass {
 			'min_pages' => 'int'
 		);
 	}
-	
+
 	/**
 	 * Returns a list of default field values.
 	 * field name => field value
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @return array
 	 */
 	public static function getDefaults() {
@@ -73,84 +73,84 @@ class Survey extends SurveyDBClass {
 			'min_pages' => SurveySettings::get( 'defaultMinPages' ),
 		);
 	}
-	
+
 	/**
-	 * Gets the db field prefix. 
-	 * 
+	 * Gets the db field prefix.
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @return string
 	 */
 	protected static function getFieldPrefix() {
 		return 'survey_';
 	}
-	
+
 	/**
 	 * Returns the Survey with specified name, or false if there is no such survey.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @param string $surveyName
 	 * @param array|null $fields
 	 * @param boolean $loadQuestions
-	 * 
+	 *
 	 * @return Survey or false
 	 */
 	public static function newFromName( $surveyName, $fields = null, $loadQuestions = true ) {
 		return self::newFromDB( array( 'name' => $surveyName ), $fields, $loadQuestions );
 	}
-	
+
 	/**
 	 * Returns the Survey with specified ID, or false if there is no such survey.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @param int $surveyId
 	 * @param array|null $fields
 	 * @param boolean $loadQuestions
-	 * 
+	 *
 	 * @return Survey or false
 	 */
 	public static function newFromId( $surveyId, $fields = null, $loadQuestions = true ) {
 		return self::newFromDB( array( 'id' => $surveyId ), $fields, $loadQuestions );
 	}
-	
+
 	/**
 	 * Returns a new instance of Survey build from a database result
 	 * obtained by doing a select with the porvided conditions on the surveys table.
 	 * If no survey matches the conditions, false will be returned.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @param array $conditions
 	 * @param array|null $fields
 	 * @param boolean $loadQuestions
-	 * 
+	 *
 	 * @return Survey or false
 	 */
 	public static function newFromDB( array $conditions, $fields = null, $loadQuestions = true ) {
 		$survey = self::selectRow( $fields, $conditions );
-		
+
 		if ( $survey !== false && $loadQuestions ) {
 			$survey->loadQuestionsFromDB();
 		}
-		
+
 		return $survey;
 	}
-	
+
 	/**
 	 * The questions that go with this survey.
-	 * 
+	 *
 	 * @since 0.1
 	 * @var array of SurveyQuestion
 	 */
 	protected $questions;
-	
+
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @param array|null $fields
 	 * @param boolean $loadDefaults
 	 * @param array $questions
@@ -159,46 +159,46 @@ class Survey extends SurveyDBClass {
 		parent::__construct( $fields, $loadDefaults );
 		$this->setQuestions( $questions );
 	}
-	
+
 	/**
 	 * Load the surveys questions from the database.
-	 * 
+	 *
 	 * @since 0.1
 	 */
 	public function loadQuestionsFromDB() {
 		$this->questions = SurveyQuestion::getQuestionsForSurvey( $this->getId() );
 	}
-	
+
 	/**
 	 * Writes the surveyand it's questions to the database, either updating it
 	 * when it already exists, or inserting it when it doesn't.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @return boolean Success indicator
 	 */
 	public function writeToDB() {
 		$success = parent::writeToDB();
-		
+
 		if ( $success ) {
 			$success = $this->writeQuestionsToDB();
 		}
-		
+
 		return $success;
 	}
-	
+
 	/**
 	 * Writes the surveys questions to the database.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @return boolean Success indicator
 	 */
 	public function writeQuestionsToDB() {
 		$success = true;
-		
+
 		$dbw = wfGetDB( DB_MASTER );
-		
+
 		$dbw->startAtomic( __METHOD__ );
 
 		SurveyQuestion::update(
@@ -213,47 +213,47 @@ class Survey extends SurveyDBClass {
 			$question->setField( 'survey_id', $this->getId() );
 			$success = $question->writeToDB() && $success;
 		}
-		
+
 		$dbw->endAtomic( __METHOD__ );
-		
+
 		return $success;
 	}
-	
+
 	/**
 	 * Returns the surveys questions.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @return array of SurveyQuestion
 	 */
 	public function getQuestions() {
 		return $this->questions;
 	}
-	
+
 	/**
 	 * Sets the surveys questions.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @param array $questions list of SurveyQuestion
 	 */
 	public function setQuestions( array /* of SurveyQuestion */ $questions ) {
 		$this->questions = $questions;
 	}
-	
+
 	/**
 	 * Serializes the survey to an associative array which
 	 * can then easily be converted into JSON or similar.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @param null|array $fields
-	 * 
+	 *
 	 * @return array
 	 */
 	public function toArray( $fields = null ) {
 		$data = parent::toArray( $fields );
-		
+
 		$data['questions'] = array();
 
 		/**
@@ -262,77 +262,77 @@ class Survey extends SurveyDBClass {
 		foreach ( $this->questions as /* SurveyQuestion */ $question ) {
 			$data['questions'][] = $question->toArray();
 		}
-		
+
 		return $data;
 	}
-	
+
 	/**
 	 * Removes the object from the database.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @return boolean Success indicator
 	 */
 	public function removeFromDB() {
-		
 		$dbr= wfgetDB( DB_REPLICA );
+
 		$submissionsForSurvey = $dbr->select(
 			'survey_submissions',
 			array( 'submission_id' ),
 			array( 'submission_survey_id' => $this->getId() )
 		);
-		
+
 		$dbw = wfGetDB( DB_MASTER );
-		
+
 		$dbw->startAtomic( __METHOD__ );
-		
+
 		$sucecss = parent::removeFromDB();
-		
+
 		$sucecss = $dbw->delete(
 			'survey_questions',
 			array( 'question_survey_id' => $this->getId() )
 		) && $sucecss;
-		
+
 		$sucecss = $dbw->delete(
 			'survey_submissions',
 			array( 'submission_survey_id' => $this->getId() )
 		) && $sucecss;
-		
+
 		foreach ( $submissionsForSurvey as $nr => $submission ) {
 			$sucecss = $dbw->delete(
 				'survey_answers',
 				array( 'answer_submission_id' => $submission->id )
 			) && $sucecss;
 		}
-		
+
 		$dbw->endAtomic( __METHOD__ );
-		
+
 		return $sucecss;
 	}
-	
+
 	/**
 	 * Returns the survey user types the provided user has.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @param User $user
-	 * 
+	 *
 	 * @return array of Survey::$USER_
 	 */
 	public static function getTypesForUser( User $user ) {
 		$userTypes = array( Survey::$USER_ALL );
-		
+
 		$userTypes[] = $user->isLoggedIn() ? Survey::$USER_LOGGEDIN : Survey::$USER_ANON;
-		
+
 		if ( $user->isEmailConfirmed() ) {
 			$userTypes[] = Survey::$USER_CONFIRMED;
 		}
-		
+
 		if ( $user->getEditCount() > 0 ) {
 			$userTypes[] = Survey::$USER_EDITOR;
 		}
-		
+
 		return $userTypes;
 	}
-	
+
 }
