513e8ddcd234557355a0776e37e1cd57c0f1958e
Func <Funcer@outlook.com>
2022-03-20 18:01:25 +0800

    SpecialSurveys: Avoid setting edit token in HTMLForm twice
    HTMLForm would set it for post form, use setTokenSalt() to amend.
    
    Change-Id: Ibf26a282915e32baf4b486c3fb1a64eb6a809fb9

--- specials/SpecialSurveys.php
+++ specials/SpecialSurveys.php
@@ -80,9 +80,7 @@ class SpecialSurveys extends SpecialSurveyPage {
 
 		$htmlForm = HTMLForm::factory( 'ooui', $formDescriptor, $this->getContext() );
 		$htmlForm
-			->addHiddenField( 'wpEditToken', $this->getUser()->getEditToken() )
 			->setHeaderText( $this->msg( 'surveys-special-namedoc' )->text() )
-			->setMethod( 'post' )
 			->setAction( $this->getPageTitle()->getLocalURL() )
 			->setSubmitName( 'addnewsurvey' )
 			->setSubmitTextMsg( 'surveys-special-add' )
