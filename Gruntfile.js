2fce28776981bec6439d0854270fab19fcaaad3f
zoranzoki21 <zorandori4444@gmail.com>
2023-11-08 03:36:35 +0100

    Tweak Grunt and add stylelint
    Change-Id: I770ca3ac1ca50f4e8011561390526087efd7c1db

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,13 @@
-/* eslint-env node, es6 */
+/* eslint-env node */
 module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
+
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
+		banana: conf.MessagesDirs,
 		eslint: {
 			options: {
 				cache: true
@@ -16,11 +20,16 @@ module.exports = function ( grunt ) {
 				'!vendor/**'
 			]
 		},
-		banana: {
-			all: 'i18n/'
+		stylelint: {
+			all: [
+				'**/*.{css,less}',
+				'!resources/fancybox/**',
+				'!node_modules/**',
+				'!vendor/**'
+			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'eslint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'banana', 'stylelint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
